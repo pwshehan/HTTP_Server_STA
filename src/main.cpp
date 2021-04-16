95cb421a2ab0f4f92330bf230d8b03b45303a8f3
Shehan Pramitha <pwshehan@gmail.com>
2021-04-16 16:18:14 +0530

    env.h file

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,17 @@
 #include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+
+#include <../env.h> // Environment file
+
+void setup()
+{
+  Serial.begin(9600);
 }
 
-void loop() {
+void loop()
+{
+  Serial.println(ssid);
   // put your main code here, to run repeatedly:
 }
